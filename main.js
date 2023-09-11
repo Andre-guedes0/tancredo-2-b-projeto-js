480d7aec1c580b326f064ffe2753384e002f0500
Andre-guedes0 <137520157+Andre-guedes0@users.noreply.github.com>
2023-09-11 10:46:37 -0300

    11/09 uuuuuuuf

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-alert('Olá Mundo');
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    console.log(instrumento)
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
